69ee65ecc7ecb3f4b4842f585087870d6c69da34
f4ust0f <f4ust0f@713fc61c-ca35-11de-a7ce-03bda429b547>
2010-02-16 10:24:14 +0000

    changed QOSGWidget

--- branches/dev/Components/OsgCore/source/QOSGWidget.h
+++ branches/dev/Components/OsgCore/source/QOSGWidget.h
@@ -15,29 +15,62 @@
 *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 *  THE SOFTWARE.
 */
-#ifndef _QOSG_WIDGET_HPP_
-#define _QOSG_WIDGET_HPP_
 
 
-#include <QtCore/QString>
-//#include <QtCore/QTimer>
-#include <QtGui/QKeyEvent>
-#include <QtGui/QApplication>
+#define USE_QT4 1
 
-#include <QtGui/QWidget>
-#include <QtGui/QMouseEvent>
-#include <QtGui/QFocusEvent>
+#include <osg/Config>
 
-    
-using Qt::WindowFlags;
+#if defined(_MSC_VER) && defined(OSG_DISABLE_MSVC_WARNINGS)
+    // disable warning "'QtConcurrent::BlockSizeManager' : assignment operator could not be generated"
+    #pragma warning( disable : 4512 )
+#endif
 
-// Port Note 10/14/08 LM -- I tried putting some of these headers into the
-// source file, but ran into compile problems.  Very order dependent?
-#include <osgViewer/View>
+#if USE_QT4
+
+    #include <QtCore/QString>
+    #include <QtCore/QTimer>
+    #include <QtGui/QKeyEvent>
+    #include <QtGui/QApplication>
+    #include <QtGui/QtGui>
+    #include <QtGui/QWidget>
+	#include <QtGui/QPaintEngine>
+    using Qt::WindowFlags;
+
+#else
+
+    class QWidget;
+    #include <qtimer.h>
+    #include <qgl.h>
+    #include <qapplication.h>
+
+    #define WindowFlags WFlags
+
+#endif
+
+
 #include <osgViewer/Viewer>
 #include <osgViewer/CompositeViewer>
 #include <osgViewer/ViewerEventHandlers>
 #include <osgViewer/GraphicsWindow>
+
+#include <osgViewer/ViewerEventHandlers>
+
+#if defined(WIN32) && !defined(__CYGWIN__)
+#include <osgViewer/api/Win32/GraphicsWindowWin32>
+typedef HWND WindowHandle;
+typedef osgViewer::GraphicsWindowWin32::WindowData WindowData;
+
+#elif defined(__APPLE__)  // Assume using Carbon on Mac.
+#include <osgViewer/api/Carbon/GraphicsWindowCarbon>
+typedef WindowRef WindowHandle;
+typedef osgViewer::GraphicsWindowCarbon::WindowData WindowData;
+#else // all other unix
+#include <osgViewer/api/X11/GraphicsWindowX11>
+typedef Window WindowHandle;
+typedef osgViewer::GraphicsWindowX11::WindowData WindowData;
+#endif
+
 
 #include <osgGA/TrackballManipulator>
 #include <osgGA/FlightManipulator>
@@ -46,118 +79,163 @@ using Qt::WindowFlags;
 #include <osgGA/StateSetManipulator>
 #include <osgGA/AnimationPathManipulator>
 #include <osgGA/TerrainManipulator>
+#include <osg/ShapeDrawable>
+#include <osg/Geode>
 
 #include <osgDB/ReadFile>
 
 #include <iostream>
+#include <sstream>
 
 class QOSGWidget : public QWidget
 {
-public:
+    public:
 
-    QOSGWidget( QWidget * parent = 0, WindowFlags f = 0 );
+        QOSGWidget( QWidget * parent = 0, const char * name = 0, WindowFlags f = 0, bool overrideTraits = false);
 
-    virtual ~QOSGWidget() {}
+        virtual ~QOSGWidget() {}
 
-    osgViewer::GraphicsWindow* getGraphicsWindow() { 
-        return _gw.get(); 
-    }
-    const osgViewer::GraphicsWindow* getGraphicsWindow() const { 
-        return _gw.get(); 
-    }
+        osgViewer::GraphicsWindow* getGraphicsWindow() { return _gw.get(); }
+        const osgViewer::GraphicsWindow* getGraphicsWindow() const { return _gw.get(); }
 
-protected:
+    protected:
 
-    void init();
-    void createContext( QWidget *qwindow = 0 );
-    bool eventFilter(QObject *obj, QEvent *event);
+        void init();
+        void createContext();
 
-    // Looking at the doc for Qt::WA_PaintOnScreen this may be appropriate.
-    // Didn't seem to help or hurt.
-    virtual QPaintEngine *paintEngine() { return 0; }
-
-// Grabbed this from Martin Beckett:
-//  The GraphincsWindowWin32 implementation already takes care of message handling.
-//  We don't want to relay these on Windows, it will just cause duplicate messages
-//  with further problems downstream (i.e. not being able to throw the trackball
-
-#ifndef WIN32
-    virtual void mouseDoubleClickEvent ( QMouseEvent * event );
-    virtual void closeEvent( QCloseEvent * event );
-    virtual void destroyEvent( bool destroyWindow = true, 
-                               bool destroySubWindows = true);
-    virtual void resizeEvent( QResizeEvent * event );
-    virtual void mousePressEvent( QMouseEvent* event );
-    virtual void mouseReleaseEvent( QMouseEvent* event );
-    virtual void mouseMoveEvent( QMouseEvent* event );
-#endif // ndef WIN32
-    virtual void keyPressEvent( QKeyEvent* event );
-    virtual void keyReleaseEvent( QKeyEvent* event );
-
-    osg::ref_ptr<osgViewer::GraphicsWindow> _gw;
-    bool _overrideTraits;
-
-}; // QOSGWidget
-
-
-//------------------------------------------------------------------------------
-// I could get Linux to work in so many different ways.
-//
-// But I could get Windows to work in only one way:
-//    1.  If QOSGWidget is constructed with a parent QWidget, the parent's
-//        focusPolicy is set to StrongFocus.  The QOSGWidget widget 
-//        will have NoFocus, the default for QWidget.
-//
-//    2.  If ViewQOSG is part of a osgViewer::CompositeViewer,
-//        ViewQOSG::keyReleaseEvent(), on a TAB or Shift-TAB key release,
-//        sets the Viewer's cameraWithFocus to the ViewQOSG's camera.
-//        All other key releases are passed up to QOSGWidget::keyReleaseEvent.
-//
-//    3.  Since the QOSGWidget's focusPolicy is noFocus, and hence ViewQOSG's
-//        is as well, ViewQOSG::focusInEvent() will never be called.
-//
-//    This was the only way I could get Windows to allow tabbing to change
-//    which View of the CompositeViewer should have focus.
-//    
-//    Using StrongFocus on the QOSGWidget (and hence ViewQOSG) instead of its
-//    parent caused all sorts of behavioral problems on Windows.  For example,
-//    tabbing didn't work as expected.  Or it did, but it didn't matter
-//    because the CompositeViewer considered the "current ViewQOSG" to be the
-//    one under the mouse.
-//    
-//    The code we based this on created a class that inherited from
-//    QOSGWidget and osgViewer::Viewer.  We instead inherit from 
-//    QOSGWidget and osgViewer::View.  This allows us to post a different
-//    scene graph in each view that the viewer manages.
-
-class ViewQOSG : public osgViewer::View, public QOSGWidget
-{
-public:
-    ViewQOSG( QWidget *parent /*, osg::GraphicsContext::Traits* traits*/ );
-
-    virtual ~ViewQOSG() {}
-
-// Reimplement from QWidget
+        //  The GraphincsWindowWin32 implementation already takes care of message handling.
+        //  We don't want to relay these on Windows, it will just cause duplicate messages
+        //  with further problems downstream (i.e. not being able to throw the trackball
 #ifndef WIN32 
-    virtual void resizeEvent( QResizeEvent * event );
-#endif // ndef WIN32
-    virtual void keyReleaseEvent( QKeyEvent* event );
-
-    void setData( osg::ref_ptr<osg::Node> loadedModel );
-
-    QWidget *         getDrawingAreaWidget()  { return _daw; }
-
-    float aspectRatio( int width, int height );
-
-protected:
-    void        focusInEvent( QFocusEvent *event );
-
-    QWidget *_daw;  // drawing area widget
-    int _x, _y, _width, _height;
-
+        virtual void mouseDoubleClickEvent ( QMouseEvent * event );
+        virtual void closeEvent( QCloseEvent * event );
+        virtual void destroyEvent( bool destroyWindow = true, bool destroySubWindows = true);
+        virtual void resizeEvent( QResizeEvent * event );
+        virtual void keyPressEvent( QKeyEvent* event );
+        virtual void keyReleaseEvent( QKeyEvent* event );
+        virtual void mousePressEvent( QMouseEvent* event );
+        virtual void mouseReleaseEvent( QMouseEvent* event );
+        virtual void mouseMoveEvent( QMouseEvent* event );
+#endif
+        osg::ref_ptr<osgViewer::GraphicsWindow> _gw;
+    bool _overrideTraits;
 };
 
 
-extern void setupManipulatorAndHandler(osgViewer::View & viewer
-                                       /*, osg::ArgumentParser & arguments*/);
-#endif // _QOSG_WIDGET_HPP_
+
+
+class ViewerQOSG : public osgViewer::Viewer, public QOSGWidget
+{
+    public:
+
+        ViewerQOSG(QWidget * parent = 0, const char * name = 0, WindowFlags f = 0):
+            QOSGWidget( parent, name, f )
+        {
+            setThreadingModel(osgViewer::Viewer::SingleThreaded);
+
+            connect(&_timer, SIGNAL(timeout()), this, SLOT(update()));
+            _timer.start(10);
+        }
+        
+        void updateCamera()
+        {
+            getCamera()->setViewport(new osg::Viewport(0,0,width(),height()));
+            getCamera()->setProjectionMatrixAsPerspective(30.0f, static_cast<double>(width())/static_cast<double>(height()), 1.0f, 10000.0f);
+            getCamera()->setGraphicsContext(getGraphicsWindow());
+        }
+
+        virtual void paintEvent( QPaintEvent * event ) { frame(); }
+
+    protected:
+
+        QTimer _timer;
+};
+
+
+class CompositeViewerQOSG : public osgViewer::CompositeViewer, public QOSGWidget
+{
+    public:
+        CompositeViewerQOSG(QWidget * parent = 0, const char * name = 0, WindowFlags f = 0)
+        : QOSGWidget( parent, name, f )
+        {
+          setThreadingModel(osgViewer::CompositeViewer::SingleThreaded);
+
+          connect(&_timer, SIGNAL(timeout()), this, SLOT(repaint()));
+
+          // The composite viewer needs at least one view to work
+          // Create a dummy view with a zero sized viewport and no
+          // scene to keep the viewer alive.
+          osgViewer::View * pView = new osgViewer::View;
+          pView->getCamera()->setGraphicsContext( getGraphicsWindow() );
+          pView->getCamera()->setViewport( 0, 0, 0, 0 );
+          addView( pView );
+
+          // Clear the viewer of removed views
+          getGraphicsWindow()->setClearMask( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+          getGraphicsWindow()->setClearColor( osg::Vec4( 0.08, 0.08, 0.5, 1.0 ) );
+
+          // The app would hang on exit when using start(1).  Behaves better with 10
+          // like the non-composite viewer.  Was this just a typo?
+          _timer.start(10);
+        }
+          
+        virtual void paintEvent( QPaintEvent * event ) { frame(); }
+
+        void keyPressEvent( QKeyEvent* event )
+        {
+          if ( event->text() == "a" )
+          {
+            AddView( _scene.get() );
+          }
+          
+          if ( event->text() == "r" )
+          {
+            RemoveView();
+          }
+
+          QOSGWidget::keyPressEvent( event );
+        }
+
+
+        void AddView( osg::Node * scene );
+        void RemoveView();
+        void Tile();
+
+        osg::ref_ptr< osg::Node > _scene;
+
+    protected:
+        QTimer _timer;
+};
+
+
+
+#if USE_QT4
+// we use this wrapper for CompositeViewer ONLY because of the timer
+// NOTE: this is a workaround because we're not using QT's moc precompiler here.
+//
+class QViewerTimer : public QWidget
+{
+
+    public:
+
+        QViewerTimer (QWidget * parent = 0, WindowFlags f = 0):
+            QWidget (parent, f)
+    {
+        _viewer = new osgViewer::CompositeViewer ();
+        _viewer->setThreadingModel(osgViewer::CompositeViewer::DrawThreadPerContext);
+        connect(&_timer, SIGNAL(timeout()), this, SLOT(repaint()));
+        _timer.start(10);
+    }
+
+    ~QViewerTimer ()
+    {
+        _timer.stop ();
+    }
+
+        virtual void paintEvent (QPaintEvent * event) { _viewer->frame(); }
+
+        osg::ref_ptr <osgViewer::CompositeViewer> _viewer;
+        QTimer _timer;
+
+};
+#endif
