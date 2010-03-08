ba852bc98e8583b8863b3acc842a03b04053a8f4
f4ust0f <f4ust0f@713fc61c-ca35-11de-a7ce-03bda429b547>
2010-03-08 14:20:38 +0000

    possible to easily add new viewports types (tree, list, table...).

--- branches/dev/Components/OsgCore/source/ViewportPanel.h
+++ branches/dev/Components/OsgCore/source/ViewportPanel.h
@@ -1,75 +1,15 @@
 #ifndef VIEWPORT_PANEL_H
 #define VIEWPORT_PANEL_H
 
-#include <QTabWidget>
-#include <QWidget>
-#include <QMap>
 #include <QSplitter>
 #include <QVBoxLayout>
 #include <QToolBar>
 #include <QComboBox>
-#include <QAction>
 #include <QStackedWidget>
-#include <osgViewer/View>
-#include "CompassAxis.h"
+#include "QViewWidget.h"
 
 class ViewportsSplitter;
-class CompassAxis;
-
-/**
- * The number of lines in each direction.
- */
-const int GRID_LINE_COUNT = 49;
-
-/**
- * The amount of space between each line.
- */
-const float GRID_LINE_SPACING = 1.0f;
-
-struct ViewWidget {
-public:
-	ViewWidget(QWidget * widget, osgViewer::View * view, osg::Group * sceneRoot, bool showGrids = true, bool showAxes = true);
-	~ViewWidget();
-
-	QWidget * getWidget() const {return _widget;}
-	osgViewer::View * getView() const {return _view;}
-	osg::Group * getRoot() const {return _sceneRoot;}
-
-	bool valid() const {return _sceneRoot != 0;}
-
-	CompassAxis * getAxes() const {return compassAxes;}
-	bool showGrids() const {return _showGrids;}
-	bool showAxes() const {return _showAxes;}
-
-	void toggleXYGrid();
-	void toggleXZGrid();
-	void toggleYZGrid();
-	void toggleAxes();
-
-	osg::MatrixTransform* getXYGrid() const {return mpXYGridTransform;}
-	osg::MatrixTransform* getXZGrid() const {return mpXZGridTransform;}
-	osg::MatrixTransform* getYZGrid() const {return mpYZGridTransform;}
-
-private:
-	QWidget * _widget;
-	osgViewer::View * _view;
-	osg::Group * _sceneRoot;
-
-	bool xyGridToggled;
-	bool xzGridToggled;
-	bool yzGridToggled;
-	bool axesToggled;
-
-	void initGrids();
-	bool _showGrids;
-	void initAxes();
-	bool _showAxes;
-
-	osg::MatrixTransform* mpXYGridTransform;
-	osg::MatrixTransform* mpXZGridTransform;
-	osg::MatrixTransform* mpYZGridTransform;
-	CompassAxis * compassAxes;
-};
+class QViewWidget;
 
 class ViewportPanel : public QWidget
 {
@@ -85,33 +25,31 @@ public:
 	void updateIndexLayout();
 
 public slots:
-	void setSelectedScene(int index);
-	void setSelectedCamera(int index);
-	void setSelectedDisplay(int index);
+	// select type (view, tree, table, data)
+	void setSelectedType(int index);
 
-	void toggleXYGrid();
-	void toggleAxes();
+	// select viewport (single, quad, top-bottom...)
 	void setViewport(int index);
 	void updateIndexLayout(const int& index);
 
 private:
-	QMap<QString, ViewWidget*> viewMap;
-
 	QVBoxLayout sceneLayout;
-	QStackedWidget sceneStack;
-	QToolBar sceneToolBar;
-	QComboBox sceneSelection;
-	QComboBox sceneCameras;
-	QComboBox sceneDisplays;
+
+	QToolBar mainToolBar;
+	// stacked widgets used to switch from a typeOfView/scene and
+	// related widgets to another typeOfView/scene 
+	QStackedWidget toolStack;
+	QStackedWidget centralStack;
+
+	// global widgets
+	QComboBox typeOfView;
 	QComboBox sceneViewports;
 
-	ViewportsSplitter * splitter;
+	// scene view widgeta/actions
+	QViewWidget * sceneWidgets;
 
-	// Actions for toolbar
-	QAction * showgridAction;
-	QAction * showgizmoAction;
-	QAction * showaxisAction;
-	QAction * saveviewAction;
+	// same for tree, table, list...
+	ViewportsSplitter * splitter;
 };
 
 #endif
