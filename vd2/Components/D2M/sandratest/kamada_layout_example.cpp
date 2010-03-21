1c931675f18310e1cf88dc860e2a8c6a5d8b903a
sandrasturesson <sandrasturesson@hotmail.com>
2010-03-21 16:25:49 +0000

    Added plotting of the edges between vertices

--- vd2/Components/D2M/sandratest/kamada_layout_example.cpp
+++ vd2/Components/D2M/sandratest/kamada_layout_example.cpp
@@ -49,22 +49,45 @@
 			osg::notify(osg::FATAL) << "Failed in osgDB::writeNodeFile().\n";
 	}
 
+	osg::Cylinder *createCylinder(osg::Vec3 startPoint, osg::Vec3 endPoint)
+	{
+		const osg::Vec3 rotAxis = osg::Vec3(0.0f, 1.0f, 0.0f); //Axis around which the cylinder will be rotated
+		float radius = 0.2f;
+		osg::Vec3 center = (startPoint + endPoint)/2;
+		osg::Vec3 diff = endPoint - startPoint;
+		float height = diff.length();
+		float angle = std::atan( diff.x()/diff.z() );
+
+		osg::Cylinder *cylinder = new osg::Cylinder( center, radius, height );
+		cylinder->setRotation( osg::Quat( angle, rotAxis ) );
+		return cylinder;
+	}
+
 	void constructScene(Graph &g, PositionMap &positionMap)
 	{
-		
-		//Create a cube for each vertex and add all cubes to a Geode
+		//Create a cube for each vertex and a cylinder for each edge
+		//Add all cubes and cylinders to a Geode
 		float cubeSize = 1.f;
-		osg::Vec3 position;
+		osg::Vec3 position1;
+		osg::Vec3 position2;
 		osg::ref_ptr<osg::ShapeDrawable> cube;
+		osg::ref_ptr<osg::ShapeDrawable> line;
 		osg::ref_ptr<osg::Geode> geode = new osg::Geode;
 		
 		boost::graph_traits<Graph>::vertex_iterator i, end;
+		boost::graph_traits<Graph>::out_edge_iterator ei, edge_end;
 		for (boost::tie(i, end) = boost::vertices(g); i != end; ++i) {
-			position = osg::Vec3( positionMap[*i][0], 0.f, positionMap[*i][1] );
-			cube = new osg::ShapeDrawable( new osg::Box(position, cubeSize) );
-		    geode->addDrawable(cube);
+			position1 = osg::Vec3( positionMap[*i][0], 0.f, positionMap[*i][1] );
+			cube = new osg::ShapeDrawable( new osg::Box(position1, cubeSize) );
+			geode->addDrawable(cube);
+			for (boost::tie(ei,edge_end) = out_edges(*i, g); ei != edge_end; ++ei)
+			{
+				position2 = osg::Vec3( positionMap[target(*ei, g)][0], 0.f, positionMap[target(*ei, g)][1] );
+				line = new osg::ShapeDrawable( createCylinder(position1, position2) );
+				geode->addDrawable(line);
+			}
 		}
-		
+
 		//Save the Geode in an osg file
 		saveOsgFile(geode);
 	}
@@ -90,7 +113,7 @@
 		add_edge(vertex(5,g), vertex(6,g), EdgeProperty(1), g);
 		add_edge(vertex(6,g), vertex(7,g), EdgeProperty(1), g);
 		add_edge(vertex(7,g), vertex(0,g), EdgeProperty(1), g);
-			
+
 
 		// Create property maps for vertex position and for edge weight
 		PositionMap positionMap = boost::get(&VertexProperties::point, g);
