b0d31f9a824b6ca61d4296a38c5642964dd7d8e6
jbellone <jbellone@unog.ch>
2010-03-21 21:46:29 +0000

    Solution now compiles...stand alone with link to framework core. This solution is for node development.

--- nodekits/plugins/panels/PatternExplorerPanel/PatternExplorerPanel.cpp
+++ nodekits/plugins/panels/PatternExplorerPanel/PatternExplorerPanel.cpp
@@ -38,6 +38,9 @@ http://www.gnu.org/copyleft/lesser.txt.
 #include <DoubleSlider.h>
 #include <NodeFactory.h>
 #include <Log.h>
+
+#include <QTreeView>
+#include <QFileSystemModel>
 /*
 #include <QtGui/QTabWidget>
 #include <QtGui/QLabel>
@@ -71,35 +74,21 @@ Q_DECLARE_METATYPE(Ogre::Vector3)
 PatternExplorerPanel::PatternExplorerPanel ( QWidget *parent /* = 0 */, Qt::WindowFlags flags /* = 0 */ ) :
     ViewPanel(ViewPanel::T_PluginPanel, parent, flags)
 {
+	QString templatesDirectory;
+	QTreeView* dirView;
+	QFileSystemModel* dirModel;
+	// New Dir Browser
+	dirView = new QTreeView(this);
+	dirModel = new QFileSystemModel;
 
-	// Creation of labels and List Widgets
+	templatesDirectory = "patterns";
 
-	QLabel *labelNodes = new QLabel("Nodes:", this);
-	labelNodes->setGeometry(20, 10,100,20);
+	dirModel->setRootPath(templatesDirectory);
+	dirView->setModel(dirModel);
+	dirView->setRootIndex(dirModel->setRootPath(templatesDirectory));
 
-	m_ListNodes = new QListWidget(this);
-	m_ListNodes->setGeometry(10, 30, 100, 280);
+	dirView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-	QLabel *labelInputParameters = new QLabel("Input Parameters:", this);
-	labelInputParameters->setGeometry(160, 10, 150,20);
-	m_InputParameters = new QListWidget(this);
-	m_InputParameters->setGeometry(150, 30, 400, 80);
-
-	QLabel *labelParameters = new QLabel("Parameters:", this);
-	labelParameters->setGeometry(160,110,150,20);
-	m_NodeParameters = new QListWidget(this);
-	m_NodeParameters->setGeometry(150, 130, 400, 80);
-
-	QLabel *labelOutputParameters = new QLabel("Output Parameters:", this);
-	labelOutputParameters->setGeometry(160,210,150,20);
-	m_OutputParameters = new QListWidget(this);
-	m_OutputParameters->setGeometry(150, 230, 400, 80);
-
-	// connection of a signal to a slot
-
-	QObject::connect(m_ListNodes, SIGNAL(itemPressed(QListWidgetItem *)), this, SLOT(nodeSelected(QListWidgetItem *)));
-
-	m_description = false;
 }
 
 
@@ -112,10 +101,7 @@ PatternExplorerPanel::PatternExplorerPanel ( QWidget *parent /* = 0 */, Qt::Wind
 //!
 PatternExplorerPanel::~PatternExplorerPanel ()
 {
-	delete m_ListNodes;
-	delete m_InputParameters;
-	delete m_NodeParameters;
-	delete m_OutputParameters;
+
 }
 
 ///
@@ -133,12 +119,6 @@ void PatternExplorerPanel::registerControl(NodeModel *nodeModel, SceneModel *sce
 {
 	m_nodeModel = nodeModel;
 	m_sceneModel = sceneModel;
-	
-	QObject::connect(m_nodeModel, SIGNAL(nodeSelected(Node *)), this, SLOT(updateSelectedNode(Node *)));
-	QObject::connect(m_sceneModel, SIGNAL(modified()), this, SLOT(update()));
-	QObject::connect(m_sceneModel, SIGNAL(objectCreated(const QString &)), this, SLOT(update()));
-	QObject::connect(m_sceneModel, SIGNAL(selectionChanged(bool)), this, SLOT(update()));
-	
 	update();	
 }
 
@@ -176,67 +156,7 @@ void PatternExplorerPanel::fillToolBars ( QToolBar *mainToolBar, QToolBar *panel
 //!
 void PatternExplorerPanel::updateSelectedNode(Node * selectedNode)
 {
-	m_node = selectedNode;
 
-	m_InputParameters->clear();
-	m_OutputParameters->clear();
-	m_NodeParameters->clear();
-
-	QString nodeName = selectedNode->getName();
-	QString typeName = selectedNode->getTypeName();
-
-	const AbstractParameter::List *parameterList = selectedNode->getParameterRoot()->getParameterList();
-
-	 for (int i = 0; i < parameterList->size(); ++i) {
-		AbstractParameter *abstractParameter = parameterList->at(i);
-		if (abstractParameter->isGroup()) {
-            ParameterGroup *parameterGroup = dynamic_cast<ParameterGroup *>(abstractParameter);
-			QString groupName = parameterGroup->getName();
-
-			QListWidgetItem *newItem = new QListWidgetItem;
-			newItem->setText(groupName);
-			newItem->setIcon(QIcon(":/hierarchyEditorIcon"));
-			newItem->setStatusTip("Parameter Group");
-			newItem->setBackgroundColor(QColor(210,210,210));
-
-			const AbstractParameter::List *abstractParameterGroup =  parameterGroup->getParameterList();
-
-			QListWidgetItem *subItem = NULL;
-			Parameter::PinType pinType = Parameter::PT_None;
-
-			for(int j = 0; j < abstractParameterGroup->size(); ++j){
-				AbstractParameter *abstractParameterInGroup = abstractParameterGroup->at(j);
-				Parameter *parameterInGroup = dynamic_cast<Parameter *>(abstractParameterInGroup);
-				pinType = parameterInGroup->getPinType();
-				subItem = addItem(parameterInGroup, true);
-			}	
-
-			QListWidget *list;
-	
-			if(pinType == Parameter::PT_Input)
-				list = m_InputParameters;	
-			
-			if(pinType == Parameter::PT_Output)
-				list = m_OutputParameters;
-			
-			if(pinType == Parameter::PT_None)
-				list = m_NodeParameters;
-				
-			int row = list->row(subItem) - abstractParameterGroup->size();
-			row++;
-
-			if(row < 0)
-				row = 0;
-
-			list->insertItem(row, newItem);
-
-		} else {
-			Parameter *parameter = dynamic_cast<Parameter *>(abstractParameter);
-			addItem(parameter, false);	
-		}
-	 }
-
-	 update();
 }
 
 //!
@@ -246,127 +166,13 @@ void PatternExplorerPanel::updateSelectedNode(Node * selectedNode)
 //! \param groupElement true if element of a parameter group
 //! \return the created QListWidgetItem
 //!
-QListWidgetItem* PatternExplorerPanel::addItem(Parameter *parameter, bool groupElement)
-{
-	QString parameterName = parameter->getName();
-	Parameter::PinType pin = parameter->getPinType();
-	Parameter::Type parameterType = parameter->getType();
-	bool parameterConnected = parameter->isConnected();
 
-	QListWidgetItem *newItem = new QListWidgetItem;
-
-	QString description;	
-
-	switch(parameterType){
-		case(Parameter::T_Bool):
-			description = QString(" (bool)");
-			newItem->setIcon(QIcon(":/preferencesIcon"));
-		break;
-		case(Parameter::T_Camera):
-			description = QString(" (camera)");
-			newItem->setIcon(QIcon(":/cameraIcon"));
-		break;
-		case(Parameter::T_Float):
-			description = QString(" (float)");
-			newItem->setIcon(QIcon(":/stage9Icon"));
-		break;
-		case(Parameter::T_Int):
-			description = QString(" (integer)");
-			newItem->setIcon(QIcon(":/stage1Icon"));
-		break;
-		case(Parameter::T_Color):
-			description = QString(" (color)");
-			newItem->setIcon(QIcon(":/paletteIcon"));
-		break;
-		case(Parameter::T_Light):
-			description = QString(" (light)");
-			newItem->setIcon(QIcon(":/lightIcon"));
-		break;
-		case(Parameter::T_Geometry):
-			description = QString(" (geometry)");
-			newItem->setIcon(QIcon(":/geometryIcon"));
-		break;
-		case(Parameter::T_Image):
-			description = QString(" (image)");
-			newItem->setIcon(QIcon(":/stereoIcon"));
-		break;
-		case(Parameter::T_String):
-			description = QString(" (string)");
-			newItem->setIcon(QIcon(":/documentationIcon"));
-		break;
-		case(Parameter::T_PlugIn):
-			{
-			description = QString(" (plugin \"");
-			newItem->setIcon(QIcon(":/pluginIcon"));
-			// Use this if frapper also knows dynamic widgets
-			ParameterPlugin *parameterPlugin =  dynamic_cast<ParameterPlugin *>(parameter);
-			description.append(parameterPlugin->getCall());
-			description.append("\")");
-			}
-
-		break;
-		default:
-			description = QString(" (undefined type)");
-			newItem->setIcon(QIcon(":/tagIcon"));
-	}
-
-	if(parameterConnected)
-		description.append(QString(" (connected)"));
-
-	if(m_description)
-		parameterName.append(description);
-
-	if(groupElement){
-		parameterName.insert(0,QString("   "));
-		newItem->setBackgroundColor(QColor(230,230,230));
-	}
-
-	newItem->setText(parameterName);
-
-	if(pin == Parameter::PT_Input){
-		newItem->setToolTip("Input Parameter");
-		m_InputParameters->addItem(newItem);
-	}
-
-	if(pin == Parameter::PT_Output){
-		newItem->setToolTip("Output Parameter");
-		m_OutputParameters->addItem(newItem);
-	}
-
-	if(pin == Parameter::PT_None){
-		newItem->setToolTip("No Pin Type");
-		m_NodeParameters->addItem(newItem);
-	}	
-
-	return newItem;
-}
 
 //!
 //! Updates the panel if the node model changes
 //!
 void PatternExplorerPanel::update()
 {
-	m_ListNodes->clear();
-
-	QStringList nodeNames = m_nodeModel->getNodeNames();
-	QStringList selectedNodes = m_sceneModel->getSelectedObjects();
-
-	for(int i = 0; i < nodeNames.length(); ++i){
-		QString nodeName = nodeNames.at(i);
-		QListWidgetItem *newItem = new QListWidgetItem;
-
-		if(selectedNodes.contains(nodeNames.at(i)))
-			newItem->setBackgroundColor(QColor(150,255,150));
-
-		newItem->setText(nodeName);
-		m_ListNodes->addItem(newItem);	
-	}
-
-	if(selectedNodes.isEmpty()){
-		m_InputParameters->clear();
-		m_OutputParameters->clear();
-		m_NodeParameters->clear();
-	}
 
 }
 
@@ -375,9 +181,7 @@ void PatternExplorerPanel::update()
 //!
 void PatternExplorerPanel::nodeSelected(QListWidgetItem * listItem)
 {
-	QString nodeName = listItem->text();
-	Node *selectedNode = m_nodeModel->getNode(nodeName);
-	m_sceneModel->selectObject(nodeName);
+
 }
 
 
