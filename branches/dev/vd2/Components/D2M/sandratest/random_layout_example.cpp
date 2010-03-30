156a152447ce569bd8bd01a6cd95d65b86d77dd7
sandrasturesson <sandrasturesson@hotmail.com>
2010-03-30 09:07:05 +0000

    Added code for a cone tree layout, which can be run from sandratest/cone3D_layout_example.cpp. However, two problems exist: One is that the kamada_layout_example won't work. Another is a runtime error that appears at the very end (i.e. after the .osg file has been cretaed) when executing cone3D_layout_example.

--- branches/dev/vd2/Components/D2M/sandratest/random_layout_example.cpp
+++ branches/dev/vd2/Components/D2M/sandratest/random_layout_example.cpp
@@ -1,116 +1,32 @@
-#include <boost/property_map/property_map.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/random_layout.hpp>
-#include <boost/graph/graph_utility.hpp>
-#include <boost/graph/topology.hpp>
-
-#include <windows.h>
-#include <osg/ref_ptr>
-#include <osg/Geode>
-#include <osg/Notify>
-#include <osg/ShapeDrawable>
-#include <osgDB/WriteFile>
-
-//#include <osg/PositionAttitudeTransform>
-
-	typedef boost::square_topology<>::point_type Point;
-	struct VertexProperties
-	{
-	    std::size_t index;
-	    Point point;
-	};
-
-	typedef boost::adjacency_list<boost::listS,
-            boost::listS, boost::undirectedS,
-            VertexProperties> Graph;
-
-	typedef boost::property_map<Graph, std::size_t VertexProperties::*>::type VertexIndexPropertyMap;
-	typedef boost::property_map<Graph, Point VertexProperties::*>::type PositionMap;
-
-	typedef boost::graph_traits<Graph>::vertex_descriptor VertexDescriptor;
-
-
-	void saveOsgFile(osg::ref_ptr<osg::Node> sceneGraph) {
-		if (!sceneGraph.valid())
-			osg::notify(osg::FATAL) << "Failed in saveOsgFile\n";
-
-		bool result = osgDB::writeNodeFile( *(sceneGraph.get()), "SASN.osg" );
-		if( !result )
-			osg::notify(osg::FATAL) << "Failed in osgDB::writeNodeFile().\n";
-	}
-
-	osg::Cylinder *createCylinder(osg::Vec3 startPoint, osg::Vec3 endPoint)
-	{
-		const osg::Vec3 rotAxis = osg::Vec3(0.0f, 1.0f, 0.0f); //Axis around which the cylinder will be rotated
-		float radius = 0.5f;
-		osg::Vec3 center = (startPoint + endPoint)/2;
-		osg::Vec3 diff = endPoint - startPoint;
-		float height = diff.length();
-		float angle = std::atan( diff.x()/diff.z() );
-
-		osg::Cylinder *cylinder = new osg::Cylinder( center, radius, height );
-		cylinder->setRotation( osg::Quat( angle, rotAxis ) );
-		return cylinder;
-	}
-
-	void constructScene(Graph g, PositionMap positionMap) {
-		
-		//Create a cube for each vertex and a cylinder for each edge
-		//Add all cubes and cylinders to a Geode
-		float cubeSize = 5.f;
-		osg::Vec3 position1; //Position of vertex at beginning of edge
-		osg::Vec3 position2; //Position of vertex at end of edge
-		osg::ref_ptr<osg::ShapeDrawable> cube;
-		osg::ref_ptr<osg::ShapeDrawable> line;
-		osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-		
-		boost::graph_traits<Graph>::vertex_iterator i, end;
-		boost::graph_traits<Graph>::out_edge_iterator ei, edge_end;
-		for (boost::tie(i, end) = boost::vertices(g); i != end; ++i) {
-			position1 = osg::Vec3( positionMap[*i][0], 0.f, positionMap[*i][1] );
-			cube = new osg::ShapeDrawable( new osg::Box(position1, cubeSize) );
-			geode->addDrawable(cube);
-			for (boost::tie(ei,edge_end) = out_edges(*i, g); ei != edge_end; ++ei)
-			{
-				position2 = osg::Vec3( positionMap[target(*ei, g)][0], 0.f, positionMap[target(*ei, g)][1] );
-				line = new osg::ShapeDrawable( createCylinder(position1, position2) );
-				geode->addDrawable(line);
-			}
-		}
-		
-		//Save the Geode in an osg file
-		saveOsgFile(geode);
-	}
+#include "GraphPropertyMapper.h"
+#include "TestPlotter.h"
 
 	int main(int,char*[])
 	{
-		// declare a graph object
-		Graph g;
-		VertexIndexPropertyMap vertexIdPropertyMap = boost::get(&VertexProperties::index, g);
+		// Create a GraphPropertyMapper
+		GraphPropertyMapper graphMapper;
 
-		// add vertices to the graph object
+		// Add vertices to the GraphPropertyMapper
 		for (int i = 0; i < 7; ++i) {
-			VertexDescriptor vd = boost::add_vertex(g);
-			vertexIdPropertyMap[vd] = i;
+			graphMapper.addVertex(i);
 		}
-		
-		// add edges to the graph object
-		add_edge(vertex(0,g), vertex(1,g), g);
-		add_edge(vertex(1,g), vertex(2,g), g);
-		add_edge(vertex(2,g), vertex(3,g), g);
-		add_edge(vertex(3,g), vertex(4,g), g);
-		add_edge(vertex(4,g), vertex(5,g), g);
-		add_edge(vertex(5,g), vertex(6,g), g);
-		add_edge(vertex(6,g), vertex(0,g), g);
-		
-		// apply Boost's random layout
-		PositionMap positionMap = boost::get(&VertexProperties::point, g);
-		boost::minstd_rand gen;
-		gen.seed (static_cast<unsigned int>(std::time(0)));
-		boost::rectangle_topology<boost::minstd_rand> topo = boost::rectangle_topology<boost::minstd_rand>(gen, -50.f,-50.f,50.f,50.f);
-		boost::random_graph_layout(g, positionMap, topo);
 
-		// create an OSG scene and save it as an osg file
-		constructScene(g, positionMap);
+		// Add edges to the GraphPropertyMapper
+/*		graphMapper.addEdge(0,1);
+		graphMapper.addEdge(1,2);
+		graphMapper.addEdge(2,3);
+		graphMapper.addEdge(3,4);
+		graphMapper.addEdge(4,5);
+		graphMapper.addEdge(5,6);
+		graphMapper.addEdge(6,0);
+*/
+		// Apply the layout
+		graphMapper.setPositionTransform(graphdefs::random2D);
+		graphMapper.transformPosition();
+		
+		// Create an OSG scene and save it as an osg file
+		TestPlotter tp;
+		tp.constructScene(graphMapper.g, graphMapper.positionMap);
 		return 0;
+
 	}
--- branches/dev/vd2/Components/D2M/sandratest/source/TestPlotter.h
+++ branches/dev/vd2/Components/D2M/sandratest/source/TestPlotter.h
@@ -10,12 +10,97 @@
 #include <osg/ShapeDrawable>
 #include <osgDB/WriteFile>
 
+template<typename Graph, typename PositionMap, typename WeightMap>
 class TestPlotter
 {
 
 public:
-	void constructScene(GraphPropertyMapper::Graph &g, GraphPropertyMapper::PositionMap &positionMap, GraphPropertyMapper::WeightMap &weightMap);
 
+	void saveOsgFile(osg::ref_ptr<osg::Node> sceneGraph)
+	{
+		if (!sceneGraph.valid())
+			osg::notify(osg::FATAL) << "Failed in saveOsgFile\n";
+
+		bool result = osgDB::writeNodeFile( *(sceneGraph.get()), "SASN.osg" );
+		if( !result )
+			osg::notify(osg::FATAL) << "Failed in osgDB::writeNodeFile().\n";
+	}
+
+	osg::Cylinder *createCylinder(osg::Vec3 startPoint, osg::Vec3 endPoint, float weight)
+	{
+		const osg::Vec3 zAxis = osg::Vec3f(0.0f, 0.0f, 1.0f); //This is the original orientation of an OSG cylinder
+		float radius = weight/15;
+		osg::Vec3 center = (startPoint + endPoint)/2;
+		osg::Vec3 diff = endPoint - startPoint; //Imagined vector exactely where the cylinder is to be placed
+		float height = diff.length(); //The cylinder length is to be the same as the length of diff
+		osg::Vec3 rotAxis = zAxis ^ diff; //Axis around which the cylinder will be rotated, perpendicular both to diff and to the z axis
+		float cosAngle = zAxis * diff / height; //Project a normalized diff on the z axis
+		float angle = std::acos( cosAngle ); //The cylinder should be rotated so that it becomes parallel with diff
+
+		osg::Cylinder *cylinder = new osg::Cylinder( center, radius, height );
+		cylinder->setRotation( osg::Quat( angle, rotAxis ) );
+		return cylinder;
+	}
+
+	void constructScene(Graph &g, PositionMap &positionMap, WeightMap &weightMap)
+	{
+		//Create a cube for each vertex and a cylinder for each edge
+		//Add all cubes and cylinders to a Geode
+		float cubeSize = 0.3f;
+		float weight;
+		osg::Vec3 position1;
+		osg::Vec3 position2;
+		osg::ref_ptr<osg::ShapeDrawable> cube;
+		osg::ref_ptr<osg::ShapeDrawable> line;
+		osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+		
+		boost::graph_traits<GraphPropertyMapper<boost::directedS>::Graph>::vertex_iterator i, end;
+		boost::graph_traits<GraphPropertyMapper<boost::directedS>::Graph>::out_edge_iterator ei, edge_end;
+		for (boost::tie(i, end) = boost::vertices(g); i != end; ++i) {
+			position1 = osg::Vec3( positionMap[*i][0], positionMap[*i][1], positionMap[*i][2] );
+			cube = new osg::ShapeDrawable( new osg::Box(position1, cubeSize) );
+			geode->addDrawable(cube);
+			for (boost::tie(ei,edge_end) = out_edges(*i, g); ei != edge_end; ++ei)
+			{
+				position2 = osg::Vec3( positionMap[target(*ei, g)][0], positionMap[target(*ei, g)][1], positionMap[target(*ei, g)][2] );
+				weight = weightMap[*ei];
+				line = new osg::ShapeDrawable( createCylinder(position1, position2, weight) );
+				geode->addDrawable(line);
+			}
+		}
+
+		//Save the Geode in an osg file
+		saveOsgFile(geode);
+	}
+
+	void constructScene(Graph &g, PositionMap &positionMap)
+	{
+		//Create a cube for each vertex and a cylinder for each edge
+		//Add all cubes and cylinders to a Geode
+		float cubeSize = 1.f;
+		osg::Vec3 position1;
+		osg::Vec3 position2;
+		osg::ref_ptr<osg::ShapeDrawable> cube;
+		osg::ref_ptr<osg::ShapeDrawable> line;
+		osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+		
+		boost::graph_traits<GraphPropertyMapper<boost::directedS>::Graph>::vertex_iterator i, end;
+		boost::graph_traits<GraphPropertyMapper<boost::directedS>::Graph>::out_edge_iterator ei, edge_end;
+		for (boost::tie(i, end) = boost::vertices(g); i != end; ++i) {
+			position1 = osg::Vec3( positionMap[*i][0], positionMap[*i][1], positionMap[*i][2] );
+			cube = new osg::ShapeDrawable( new osg::Box(position1, cubeSize) );
+			geode->addDrawable(cube);
+			for (boost::tie(ei,edge_end) = out_edges(*i, g); ei != edge_end; ++ei)
+			{
+				position2 = osg::Vec3( positionMap[target(*ei, g)][0], positionMap[target(*ei, g)][1], positionMap[target(*ei, g)][2] );
+				line = new osg::ShapeDrawable( createCylinder(position1, position2, 1) );
+				geode->addDrawable(line);
+			}
+		}
+
+		//Save the Geode in an osg file
+		saveOsgFile(geode);
+	}
 };
 
 #endif
