2873aff117c0f4837a8a72ec7cb433b3d2af3471
sandrasturesson <sandrasturesson@hotmail.com>
2010-04-06 11:50:40 +0000

    Solved the runtime bug for cone_layout, by redefining the Point type in GraphPropertyMapper.h, using cube_topology instead of square_topology. Also made cosmetic changes to cone3D_layout_example.cpp and to cone_layout3D.hpp and turned the variable "grandchild" in cone_layout3D.hpp into a pointer.

--- vd2/Components/D2M/sandratest/source/cone_layout3D.hpp
+++ vd2/Components/D2M/sandratest/source/cone_layout3D.hpp
@@ -31,53 +31,58 @@ namespace graphdefs {
 	template<typename VertexListGraph, typename PositionMap>
 	void cone_graph_layout(const VertexListGraph& g, PositionMap position, typename boost::graph_traits<VertexListGraph>::vertex_descriptor rootVertex)
 	{
-		typedef typename boost::graph_traits<VertexListGraph>::vertex_descriptor vertex_descriptor;
 		const double pi = 3.1415926, zStep = -1;
-		double zVal = 0.;
-		int radius = 0;
+		double zVal = 0.; //z value incrementor. The root vertex will be placed at level z=0
+		int radius = 0; // The radius will increase for each level in the cone
 
-		std::list<vertex_descriptor> *childList1 = new std::list<vertex_descriptor>();
-		std::list<vertex_descriptor> *childList2;
+		//Declare tools for perusing the tree
+		typedef typename boost::graph_traits<VertexListGraph>::vertex_descriptor vertex_descriptor;
+		std::list<vertex_descriptor> *childList = new std::list<vertex_descriptor>();
+		std::list<vertex_descriptor> *grandchildList;
 		std::list<vertex_descriptor>::iterator childListIterator;
 		typedef typename boost::graph_traits<VertexListGraph>::vertices_size_type vertices_size_type;
-		vertices_size_type numChildren;
-		vertices_size_type i;
+		vertices_size_type numChildren, i;
 
-		(*childList1).push_back(rootVertex);
-		while ( !(*childList1).empty() )
+		//The first time around, childList1 has only one element, namely the root vertex
+		(*childList).push_back(rootVertex);
+
+		//Step through the tree one level at a time, where "level" is determined by distance to the root vertex
+		while ( !(*childList).empty() )
 		{				
 			boost::graph_traits<VertexListGraph>::out_edge_iterator ei, edge_end;
-			childList2 = new std::list<vertex_descriptor>();
+			grandchildList = new std::list<vertex_descriptor>();
 
-			numChildren = (*childList1).size();
+			numChildren = (*childList).size();
 			i = 0;
-			for(childListIterator = (*childList1).begin(); childListIterator != (*childList1).end(); childListIterator++)
+			for(childListIterator = (*childList).begin(); childListIterator != (*childList).end(); childListIterator++)
 			{
-				//Distribute all vertices of this level on a circle
+				//Distribute all this level's vertices on a circle at z=zVal
 				vertex_descriptor v = *childListIterator;
 				position[v][0] = radius * std::cos(i * 2*pi/numChildren);
 				position[v][1] = radius * std::sin(i * 2*pi/numChildren);
 				position[v][2] = zVal;
 				++i;
 
-				//Collect all vertices of the next level in the tree
+				//Collect all next level's vertices, so that you have them available in the next loop
 				for (boost::tie(ei,edge_end) = out_edges(v, g); ei != edge_end; ++ei)
 				{
-					vertex_descriptor grandchild = target(*ei, g);
-					(*childList2).push_back( grandchild );
+					vertex_descriptor *grandchild = new vertex_descriptor();
+					*grandchild = target(*ei, g);
+					(*grandchildList).push_back( *grandchild );
 				}
 			}
 
 			//Take a step down to the next level
-//			(*childList1).clear(); //is this necessary?
-			delete childList1;
-			childList1 = childList2;
-			childList2 = 0;
+//			(*childList).clear(); //is this necessary?
+			delete childList;
+			childList = grandchildList;
+			grandchildList = 0;
 
 			radius++;
 			zVal = zVal + zStep;
-		}
-		delete childList1;
+		} // end while loop
+
+		delete childList;
 	} // end function cone_graph_layout
 } // end namespace graphdefs
 
