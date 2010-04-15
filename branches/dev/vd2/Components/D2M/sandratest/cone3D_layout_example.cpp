251ce73562cbe8359ba7d66cfb09dfb1b6e00e11
sandrasturesson <sandrasturesson@hotmail.com>
2010-04-15 19:04:13 +0000

    Instead of creating a .osg file, cone3D_layout_example.cpp now creates a vtkTable with columns "NodeId", "X", "Y" and "Z".

--- branches/dev/vd2/Components/D2M/sandratest/cone3D_layout_example.cpp
+++ branches/dev/vd2/Components/D2M/sandratest/cone3D_layout_example.cpp
@@ -1,39 +1,40 @@
 #include "GraphPropertyMapper.h"
+#include "vtkTable.h"
 
-	int main(int,char*[])
-	{
-		// Create a GraphPropertyMapper
-		GraphPropertyMapper<boost::directedS> graphMapper;
+int main(int,char*[])
+{
+	// Create a GraphPropertyMapper
+	GraphPropertyMapper<boost::directedS> graphMapper;
 
-		// Add vertices to the GraphPropertyMapper
-		for (int i = 0; i < 17; ++i) {
-			graphMapper.addVertex(i);
-		}
-
-		// Add edges to the GraphPropertyMapper
-		graphMapper.addEdge(0,1,1);
-		graphMapper.addEdge(0,2,1);
-		graphMapper.addEdge(0,3,1);
-		graphMapper.addEdge(1,4,1);
-		graphMapper.addEdge(1,5,1);
-		graphMapper.addEdge(1,6,1);
-		graphMapper.addEdge(1,7,1);
-		graphMapper.addEdge(2,8,1);
-		graphMapper.addEdge(2,9,1);
-		graphMapper.addEdge(3,10,1);
-		graphMapper.addEdge(3,11,1);
-		graphMapper.addEdge(3,12,1);
-		graphMapper.addEdge(3,13,1);
-		graphMapper.addEdge(3,14,1);
-		graphMapper.addEdge(3,15,1);
-		graphMapper.addEdge(3,16,1);
-
-		// Apply the layout
-		graphMapper.setPositionTransform(graphdefs::cone3D);
-		graphMapper.transformPosition();
-		
-		// Create an OSG scene and save it as an osg file
-		graphMapper.plotScene();
-
-		return 0;
+	// Add vertices to the GraphPropertyMapper
+	for (int i = 0; i < 17; ++i) {
+		graphMapper.addVertex(i);
 	}
+
+	// Add edges to the GraphPropertyMapper
+	graphMapper.addEdge(0,1,1);
+	graphMapper.addEdge(0,2,1);
+	graphMapper.addEdge(0,3,1);
+	graphMapper.addEdge(1,4,1);
+	graphMapper.addEdge(1,5,1);
+	graphMapper.addEdge(1,6,1);
+	graphMapper.addEdge(1,7,1);
+	graphMapper.addEdge(2,8,1);
+	graphMapper.addEdge(2,9,1);
+	graphMapper.addEdge(3,10,1);
+	graphMapper.addEdge(3,11,1);
+	graphMapper.addEdge(3,12,1);
+	graphMapper.addEdge(3,13,1);
+	graphMapper.addEdge(3,14,1);
+	graphMapper.addEdge(3,15,1);
+	graphMapper.addEdge(3,16,1);
+
+	// Apply the layout
+	graphMapper.setPositionTransform(graphdefs::cone3D);
+	graphMapper.transformPosition();
+	
+	// Create an OSG scene and save it as an osg file
+	vtkTable *table = graphMapper.createVtkTable(); //instead of graphMapper.plotScene();
+
+	return 0;
+}
