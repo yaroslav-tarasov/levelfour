fffd5c4d7e455e3bbc4148473a09b99f059925d9
jbellone <jbellone@unog.ch>
2010-04-22 19:53:30 +0000

    Matching vtk parameters to layouts.

--- branches/dev/app/plugins/nodes/catalog/operators/layouts/RadialTree/RadialTreeLayouterNode.cpp
+++ branches/dev/app/plugins/nodes/catalog/operators/layouts/RadialTree/RadialTreeLayouterNode.cpp
@@ -31,7 +31,19 @@ http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 //!
 
 #include "RadialTreeLayouterNode.h"
+#include "vtkGraphLayout.h"
+#include "vtkTreeLayoutStrategy.h"
+#include "vtkTable.h"
+#include "vtkGraph.h"
+#include "vtkVertexListIterator.h"
+#include "vtkStringArray.h"
+#include "vtkDoubleArray.h"
+#include "vtkVariant.h"
 
+#include "VTKGraphParameter.h"
+#include "VTKTableParameter.h"
+
+INIT_INSTANCE_COUNTER(RadialTreeLayouterNode)
 
 ///
 /// Constructors and Destructors
@@ -45,8 +57,33 @@ http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 //! \param parameterRoot A copy of the parameter tree specific for the type of the node.
 //!
 RadialTreeLayouterNode::RadialTreeLayouterNode ( const QString &name, ParameterGroup *parameterRoot ) :
-    Node(name, parameterRoot)
+    ViewNode(name, parameterRoot),
+	m_ouputVTKTableParameterName("VTKTableOutput"),
+	m_inputVTKGraphName("VTKGraphInput"),
+	m_outputTable(0),
+	m_inGraph(0)
 {
+	setTypeName("RadialTreeLayouterNode");
+
+    // create the mandatory vtk graph input parameter 
+	VTKGraphParameter * inputVTKGraphParameter = new VTKGraphParameter(m_inputVTKGraphName);
+	inputVTKGraphParameter->setMultiplicity(1);
+    inputVTKGraphParameter->setPinType(Parameter::PT_Input);
+    inputVTKGraphParameter->setSelfEvaluating(true);
+    parameterRoot->addParameter(inputVTKGraphParameter);
+	connect(inputVTKGraphParameter, SIGNAL(dirtied()), SLOT(processOutputVTKTable()));
+
+    // create the mandatory vtk table output parameter 
+	addOutputParameter(new VTKTableParameter(m_ouputVTKTableParameterName));
+
+	// link the input parameter to the output processing
+	Parameter * outputParameter = getParameter(m_ouputVTKTableParameterName);
+    if (outputParameter) 
+	{
+		outputParameter->setProcessingFunction(SLOT(processOutputVTKTable()));
+        outputParameter->addAffectingParameter(inputVTKGraphParameter);
+	}
+    INC_INSTANCE_COUNTER
 }
 
 
@@ -59,6 +96,99 @@ RadialTreeLayouterNode::RadialTreeLayouterNode ( const QString &name, ParameterG
 //!
 RadialTreeLayouterNode::~RadialTreeLayouterNode ()
 {
+	emit destroyed();
+    DEC_INSTANCE_COUNTER
+    Log::info(QString("RadialLayouterNode destroyed."), "RadialLayouterNode::~RadialLayouterNode");
 }
+
+//!
+//! Processes the node's input data to generate the node's output table.
+//!
+void RadialTreeLayouterNode::processOutputVTKTable()
+{
+	if (updateInputGraph() != 0)
+		return;
+
+	vtkGraphLayout *layout = vtkGraphLayout::New();
+	layout->SetInput( m_inGraph );
+	vtkTreeLayoutStrategy *radialTree = vtkTreeLayoutStrategy::New();
+
+	// params for radial tree
+	radialTree->SetRadial(true);
+
+	layout->SetLayoutStrategy(radialTree);
+	layout->Update();
+
+	m_outputTable = createTableFromGraph(layout->GetOutput());
+
+	// process the output vtk table
+	VTKTableParameter * outputParameter = dynamic_cast<VTKTableParameter*>(getParameter(m_ouputVTKTableParameterName));
+
+	if (outputParameter) 
+		outputParameter->setVTKTable(m_outputTable);
+}
+
+vtkTable * RadialTreeLayouterNode::createTableFromGraph(vtkGraph *graph)
+{
+	vtkTable *myTable = vtkTable::New();
+	//Create a column named "NodeId"
+	vtkStringArray *colNodeId = vtkStringArray::New();
+	colNodeId->SetName("NodeId");
+	myTable->AddColumn(colNodeId);
+	//Create columns named "X", "Y" and "Z"
+	vtkDoubleArray *colX = vtkDoubleArray::New();
+	colX->SetName("X");
+	vtkDoubleArray *colY = vtkDoubleArray::New();
+	colY->SetName("Y");
+	vtkDoubleArray *colZ = vtkDoubleArray::New();
+	colZ->SetName("Z");
+
+	//Fill the table with data from the graph vertices
+	vtkVertexListIterator *vertices = vtkVertexListIterator::New();
+	graph->GetVertices(vertices);
+	double position[3] = {0.0, 0.0, 0.0};
+	vtkIdType i = 0;
+	while (vertices->HasNext())
+	{
+		vertices->Next();
+		//Add the vertex ID to the "NodeId" column
+		char vertexId[10];
+		sprintf(vertexId, "%d",i); //Convert the vertex ID to a string
+		colNodeId->InsertNextValue( vertexId );
+		//Add the position values to columns "X", "Y" and "Z"
+		graph->GetPoint(i, position);
+		colX->InsertNextValue( position[0] );
+		colY->InsertNextValue( position[1] );
+		colZ->InsertNextValue( position[2] );
+		++i;
+	}
+	myTable->AddColumn(colX);
+	myTable->AddColumn(colY);
+	myTable->AddColumn(colZ);
+	Log::info(QString("Number rows \"%1\" created: ").arg(myTable->GetNumberOfRows()), "SingleCycleLayouterNode::createTableFromGraph");
+	Log::info(QString("Number columns \"%1\" created: ").arg(myTable->GetNumberOfColumns()), "SingleCycleLayouterNode::createTableFromGraph");
+	return myTable;
+}
+
+//!
+//! Update the input graph
+//!
+int RadialTreeLayouterNode::updateInputGraph()
+{
+	// load the input vtk parameter 
+	VTKGraphParameter * inputParameter = dynamic_cast<VTKGraphParameter*>(getParameter(m_inputVTKGraphName));
+	if (!inputParameter->isConnected())
+		return 1;
+
+	// get the source parameter (output of source node) connected to the input parameter
+	VTKGraphParameter * sourceParameter = dynamic_cast<VTKGraphParameter*>(inputParameter->getConnectedParameter());
+
+	// get the vtk graph that comes with the source parameter and set it into the input parameter of this node
+	m_inGraph = sourceParameter->getVTKGraph();
+	inputParameter->setVTKGraph(m_inGraph);
+
+	return (m_inGraph == 0);
+}
+
 
 
--- branches/dev/app/plugins/nodes/catalog/operators/layouts/RadialTree/RadialTreeLayouterNode.h
+++ branches/dev/app/plugins/nodes/catalog/operators/layouts/RadialTree/RadialTreeLayouterNode.h
@@ -33,19 +33,16 @@ http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 #ifndef RadialTreeLayouterNode_H
 #define RadialTreeLayouterNode_H
 
-#include "Node.h"
-
-// OGRE
-#include <Ogre.h>
-#if (OGRE_PLATFORM  == OGRE_PLATFORM_WIN32)
-#include <windows.h>
-#endif
-
+#include "ViewNode.h"
+#include "vtkGraph.h"
+#include "vtkTree.h"
+#include "vtkTable.h"
+#include <QString>
 
 //!
 //! Class for a node that is used for debugging purposes.
 //!
-class RadialTreeLayouterNode : public Node
+class RadialTreeLayouterNode : public ViewNode
 {
 
     Q_OBJECT
@@ -69,6 +66,48 @@ public: // constructors and destructors
     //! variable of its parent class type.
     //!
     virtual ~RadialTreeLayouterNode ();
+
+private slots: //
+
+    //!
+    //! Processes the node's input data to generate the node's output table.
+    //!
+    void processOutputVTKTable();
+
+    //!
+    //! Update the input graph (called for example when input parameters have been dirtied)
+    //! return It returns 0 if it succeeds, otherwise 1
+    //!
+    int updateInputGraph ();
+
+protected: // functions
+
+    //!
+    //! Create a vtkTable with ID, x, y, z from an vtk graph 
+    //!
+	vtkTable * createTableFromGraph(vtkGraph *graph);
+
+private: // data
+
+    //!
+    //! The name of the input vtk graph parameter.
+    //!
+    QString m_inputVTKGraphName;
+
+    //!
+    //! The name of the output vtk table parameter.
+    //!
+    QString m_ouputVTKTableParameterName;
+	
+	//!
+    //! The output Table
+    //!
+    vtkTable * m_outputTable;
+
+	//!
+    //! The input graph
+    //!
+    vtkGraph * m_inGraph;
 
 };
 
