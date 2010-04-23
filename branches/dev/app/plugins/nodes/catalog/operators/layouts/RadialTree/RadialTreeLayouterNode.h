9ae8501bd0b7210e1a28571b2849b0a6fb0cc9b2
jbellone <jbellone@unog.ch>
2010-04-23 21:25:46 +0000

    RadialTreeLayout implementation to test vtkTree as source. To do: set up parameters to work with property editor. Also, set up Tree View with adapter.
    Many layouts are based on tree structures (using XMLTreeReader, TableToTree). Layouts should take graph or tree as structure.
    
    Other layout nodes added to spec out parameters (to ensure current framework is consistent with all approaches).

--- branches/dev/app/plugins/nodes/catalog/operators/layouts/RadialTree/RadialTreeLayouterNode.h
+++ branches/dev/app/plugins/nodes/catalog/operators/layouts/RadialTree/RadialTreeLayouterNode.h
@@ -1,33 +1,6 @@
-/*
------------------------------------------------------------------------------
-This source file is part of FRAPPER
-research.animationsinstitut.de
-sourceforge.net/projects/frapper
-
-Copyright (c) 2008-2009 Filmakademie Baden-Wuerttemberg, Institute of Animation 
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free Software
-Foundation; version 2.1 of the License.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
------------------------------------------------------------------------------
-*/
-
 //!
 //! \file "RadialTreeLayouterNode.h"
 //! \brief Header file for RadialTreeLayouterNode class.
-//!
-//! \author     Stefan Habel <stefan.habel@filmakademie.de>
-//! \version    1.0
-//! \date       18.05.2009 (last updated)
 //!
 
 #ifndef RadialTreeLayouterNode_H
@@ -39,9 +12,6 @@ http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 #include "vtkTable.h"
 #include <QString>
 
-//!
-//! Class for a node that is used for debugging purposes.
-//!
 class RadialTreeLayouterNode : public ViewNode
 {
 
@@ -80,6 +50,12 @@ private slots: //
     //!
     int updateInputGraph ();
 
+	//!
+    //! Update the input graph (called for example when input parameters have been dirtied)
+    //! return It returns 0 if it succeeds, otherwise 1
+    //!
+    int updateInputTree ();
+
 protected: // functions
 
     //!
@@ -93,6 +69,11 @@ private: // data
     //! The name of the input vtk graph parameter.
     //!
     QString m_inputVTKGraphName;
+
+	//!
+    //! The name of the input vtk tree parameter.
+    //!
+    QString m_inputVTKTreeName;
 
     //!
     //! The name of the output vtk table parameter.
@@ -108,6 +89,11 @@ private: // data
     //! The input graph
     //!
     vtkGraph * m_inGraph;
+
+	//!
+    //! The input tree
+    //!
+    vtkTree * m_inTree;
 
 };
 
