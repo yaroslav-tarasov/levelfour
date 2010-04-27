1b567f1538d27f1f436d65f551c70b1fd49344c7
jbellone <jbellone@unog.ch>
2010-04-27 15:06:32 +0000

    Minor updates on in-line documentation.

--- app/core/VTKAreaLayoutNode.cpp
+++ app/core/VTKAreaLayoutNode.cpp
@@ -1,1 +1,35 @@
+/*
+-----------------------------------------------------------------------------
+file "VTKAreaLayoutNode.cpp"
+brief Implementation file for VTKAreaLayoutNode class.
+
+version    1.0
+date       21.04.2010 (last updated)
+
+Description
+-----------
+This is the base class for all layouts of area structure
+such as treemap, treering, icicle, etc. 
+
+Inputs
+----------
+	In: vtkTree
+
+Outputs
+-------
+	Out: vtkTable
+
+Result
+------
+
+A vtkTable is produced with 4 columns: nodeId, posX, posY, posZ.
+
+Notes
+-----
+vtkAreaLayout is the core of the class. Nodes 
+extending this class are based on vtkAreaLayoutStrategy.
+
+-----------------------------------------------------------------------------
+*/
+
 #include "vtkTree.h"
