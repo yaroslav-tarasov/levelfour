91e7e495302a4d61f82ce5915e3fcf1e3d16e600
jbellone <jbellone@unog.ch>
2010-04-27 20:12:24 +0000

    Setting up mappers: position, shape, size, color, material...

--- branches/dev/app/plugins/nodes/catalog/mappers/PositionMapper/PositionMapperNode.h
+++ branches/dev/app/plugins/nodes/catalog/mappers/PositionMapper/PositionMapperNode.h
@@ -1,61 +1,29 @@
-/*
------------------------------------------------------------------------------
-This source file is part of Acuity4D
-research.animationsinstitut.de
-sourceforge.net/projects/frapper
-
-Copyright (c) 2008-2009 Filmakademie Baden-Wuerttemberg, Institute of Animation 
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free Software
-Foundation; version 2.1 of the License.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
------------------------------------------------------------------------------
-*/
-
-//!
-//! \file "PositionMapperNode.h"
-//! \brief Header file for PositionMapperNode class.
-//!
-//! \version    1.0
-//! \date       18.05.2009 (last updated)
-//!
-
 #ifndef PositionMapperNODE_H
 #define PositionMapperNODE_H
 
-#include "Node.h"
-
-// OGRE
-#include <Ogre.h>
-#if (OGRE_PLATFORM  == OGRE_PLATFORM_WIN32)
-#include <windows.h>
-#endif
+#include "GeometryNode.h"
+#include "OgreContainer.h"
+#include "Ogre.h"
+#include "OgreTools.h"
 
 
 //!
-//! Class for a Degree Centrality Measure
+//! Class representing nodes that can contain OGRE entities with animation.
 //!
-class PositionMapperNode : public Node
+class PositionMapperNode : public GeometryNode
 {
 
     Q_OBJECT
-    ADD_INSTANCE_COUNTER
+        ADD_INSTANCE_COUNTER
+
+public: // nested type definitions
 
 public: // constructors and destructors
 
     //!
     //! Constructor of the PositionMapperNode class.
     //!
-    //! \param name The name for the new node.
+    //! \param name The name to give the new mesh node.
     //! \param parameterRoot A copy of the parameter tree specific for the type of the node.
     //!
     PositionMapperNode ( const QString &name, ParameterGroup *parameterRoot );
@@ -63,12 +31,119 @@ public: // constructors and destructors
     //!
     //! Destructor of the PositionMapperNode class.
     //!
-    //! Defined virtual to guarantee that the destructor of a derived class
-    //! will be called if the instance of the derived class is saved in a
-    //! variable of its parent class type.
-    //!
-    virtual ~PositionMapperNode ();
+    ~PositionMapperNode ();
 
+public slots: //
+
+    //!
+    //! Processes the node's input data to generate the node's output table.
+    //!
+    void processScene();
+
+
+private: // functions
+
+    //!
+    //! Destroy the node's Ogre resource group.
+    //!
+    void destroyResourceGroup ();
+
+    //!
+    //! Create the node's Ogre resource group.
+    //!
+    void createResourceGroup ( const QString &name, const QString &path );
+
+    //!
+    //! Loads the animation mesh from file.
+    //!
+    //! \return True if the animation mesh was successfully loaded, otherwise False.
+    //!
+    bool loadMesh ();
+
+    //!
+    //! Retrieves the numbers of vertices and triangles from the mesh and stores
+    //! them in parameters of this node.
+    //!
+    void updateStatistics ();
+
+    //!
+    //! Removes the OGRE entity containing the mesh geometry from the scene and
+    //! destroys it along with the OGRE scene node.
+    //!
+    void destroyEntity ();
+
+    //!
+    //! Create new scene.
+    //! \return True if the scene was successfully created, otherwise False.
+    //!
+	bool createEntity(QString name, QString fileName);
+
+	//!
+    //! Remove and destroy this scene.
+    //!
+	void destroySceneNode();
+
+	//!
+    //! Remove and destroy all movable objects of this scene.
+    //! \param The scenenode to be destroyed
+    //!
+	void destroyAllAttachedMovableObjects( Ogre::SceneNode* i_pSceneNode );
+
+	//!
+    //! Remove and destroy this scene and all its children.
+    //! \param The scenenode to be destroyed
+    //!
+	void destroyAllChildren( Ogre::SceneNode* i_pSceneNode );
+
+	//!
+    //! Create new scene.
+    //! \return True if the scene was successfully created, otherwise False.
+    //!
+	bool createSceneNode();
+
+private slots: //
+
+    //!
+    //! Change function for the Geometry File parameter.
+    //!
+    void geometryFileChanged ();
+
+private: // data
+
+    //!
+    //! OGRE scene node.
+    //!
+    Ogre::SceneNode *m_sceneNode;
+
+    //!
+    //! OGRE entity.
+    //!
+    Ogre::Entity *m_entity;
+
+    //!
+    //! Old resource group name.
+    //!
+    QString m_oldResourceGroupName;
+
+    //!
+    //! The container for the entity.
+    //!
+    OgreContainer *m_entityContainer;
+
+    //!
+    //! The name of the input vtk table parameter.
+    //!
+    QString m_inputVTKTableParameterName;
+
+	//!
+    //! The name of the input geometry parameter.
+    //!
+    QString m_inputGeometryParameterName;
+
+	//!
+    //! The parameters that defines the edgeFrom/To and vertices for the graph.
+    //!
+	EnumerationParameter * idFieldParameter, * xFieldParameter, * yFieldParameter, * zFieldParameter;
 };
 
 
