c2d20b501bba56db1e679485de5d71aad94047d5
jbellone <jbellone@unog.ch>
2010-04-30 13:26:26 +0000

    vtkGraphLayout based strategies are partially implemented.

--- branches/dev/app/plugins/nodes/catalog/operators/layouts/tree/CosmicTree/CosmicTreeLayouterNode.h
+++ branches/dev/app/plugins/nodes/catalog/operators/layouts/tree/CosmicTree/CosmicTreeLayouterNode.h
@@ -1,51 +1,20 @@
-/*
------------------------------------------------------------------------------
-This source file is part of FRAPPER
-research.animationsinstitut.de
-sourceforge.net/projects/frapper
-
-Copyright (c) 2008-2009 Filmakademie Baden-Wuerttemberg, Institute of Animation 
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free Software
-Foundation; version 2.1 of the License.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
------------------------------------------------------------------------------
-*/
-
 //!
 //! \file "CosmicTreeLayouterNode.h"
 //! \brief Header file for CosmicTreeLayouterNode class.
 //!
-//! \author     Stefan Habel <stefan.habel@filmakademie.de>
 //! \version    1.0
 //! \date       18.05.2009 (last updated)
 //!
 
-#ifndef CosmicTreeLayouterNode_H
-#define CosmicTreeLayouterNode_H
+#ifndef CosmicTreeLayouterNODE_H
+#define CosmicTreeLayouterNODE_H
 
-#include "Node.h"
-
-// OGRE
-#include <Ogre.h>
-#if (OGRE_PLATFORM  == OGRE_PLATFORM_WIN32)
-#include <windows.h>
-#endif
-
+#include "VTKTreeLayoutNode.h"
 
 //!
-//! Class for a node that is used for debugging purposes.
+//! Class for a CosmicTreeLayouter
 //!
-class CosmicTreeLayouterNode : public Node
+class CosmicTreeLayouterNode : public VTKTreeLayoutNode
 {
 
     Q_OBJECT
@@ -70,6 +39,19 @@ public: // constructors and destructors
     //!
     virtual ~CosmicTreeLayouterNode ();
 
+private slots: //
+
+    void setSizeLeafNodesOnly ();
+    void setLayoutDepth ();
+	void setLayoutRoot ();
+	void setNodeSizeArrayName ();
+
+private: // data
+
+	int m_sizeLeafNodesOnly;
+	int m_layoutDepth;
+	QString m_layoutRoot;
+	QString m_nodeSizeArrayName;
 };
 
 
--- branches/dev/app/plugins/nodes/catalog/operators/layouts/tree/SpanTree/SpanTreeLayouterNode.h
+++ branches/dev/app/plugins/nodes/catalog/operators/layouts/tree/SpanTree/SpanTreeLayouterNode.h
@@ -1,51 +1,20 @@
-/*
------------------------------------------------------------------------------
-This source file is part of FRAPPER
-research.animationsinstitut.de
-sourceforge.net/projects/frapper
-
-Copyright (c) 2008-2009 Filmakademie Baden-Wuerttemberg, Institute of Animation 
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free Software
-Foundation; version 2.1 of the License.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
------------------------------------------------------------------------------
-*/
-
 //!
 //! \file "SpanTreeLayouterNode.h"
 //! \brief Header file for SpanTreeLayouterNode class.
 //!
-//! \author     Stefan Habel <stefan.habel@filmakademie.de>
 //! \version    1.0
 //! \date       18.05.2009 (last updated)
 //!
 
-#ifndef SpanTreeLayouterNode_H
-#define SpanTreeLayouterNode_H
+#ifndef SpanTreeLayouterNODE_H
+#define SpanTreeLayouterNODE_H
 
-#include "Node.h"
-
-// OGRE
-#include <Ogre.h>
-#if (OGRE_PLATFORM  == OGRE_PLATFORM_WIN32)
-#include <windows.h>
-#endif
-
+#include "VTKTreeLayoutNode.h"
 
 //!
-//! Class for a node that is used for debugging purposes.
+//! Class for a SpanTreeLayouter
 //!
-class SpanTreeLayouterNode : public Node
+class SpanTreeLayouterNode : public VTKTreeLayoutNode
 {
 
     Q_OBJECT
@@ -70,6 +39,13 @@ public: // constructors and destructors
     //!
     virtual ~SpanTreeLayouterNode ();
 
+private slots: //
+
+    void setDepthFirstSpanningTree ();
+
+private: // data
+
+	bool m_depthFirst;
 };
 
 
